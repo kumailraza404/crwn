7e99b1715407a99552cc971ec674fcb6b07f264d
kumailraza404 <kumailraza404@gmail.com>
2020-10-09 04:54:28 +0500

    checkout cart is now ready to use along with the checkout page

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import "./cart-icon.styles.scss";
+import { createStructuredSelector } from "reselect";
 
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import { selectCartItemsCount } from "../../redux/cart/cart.selector";
@@ -18,7 +19,7 @@ const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-const mapStateToProps = (state) => ({
-  itemCount: selectCartItemsCount(state),
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemsCount,
 });
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
